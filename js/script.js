a3df3077043b9399431efcd007c0df8eaa95c0a3
kleantmenkulazi <kleantmenkulazi@gmail.com>
2024-07-26 23:28:59 +0200

    Correzione di una parola scritta male

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 const startButton = document.getElementById('start-button')
 
-startButton.addEventListener('click', fuction () {
+startButton.addEventListener('click', function () {
     const gridContainer = document.getElementById('grid-container');
 
     for (let i = 1; i <= 100; i++) {
         const newCell = document.createElement('div');
         newCell.innerHTML = i;
-        newCell.addEventListener('click', fuction () {
+        newCell.addEventListener('click', function () {
 
 
             this.classList.add('clicked');
