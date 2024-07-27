c78c6b3c114d0aefcfe7f6d143f31ac563e27779
kleantmenkulazi <kleantmenkulazi@gmail.com>
2024-07-27 19:47:44 +0200

    Prova bonus

--- js/script.js
+++ js/script.js
@@ -3,9 +3,34 @@ const startButton = document.getElementById('start-button')
 startButton.addEventListener('click', function () {
     const gridContainer = document.getElementById('grid-container');
 
-    for (let i = 1; i <= 100; i++) {
+    const diffSelect = document.getElementById('difficulty');
+    console.log(diffSelect.value);
+
+    let cellsNumber = 100;
+
+    if (diffSelect.value == 2) {
+        cellsNumber = 81;
+    }
+    else if (diffSelect.value == 3) {
+        cellsNumber = 49;
+    }
+
+    gridContainer.innerHTML = '';
+
+    for (let i = 1; i <= cellsNumber; i++) {
         const newCell = document.createElement('div');
         newCell.innerHTML = i;
+
+        if (cellsNumber == 100) {
+            newCell.classList.add('easy');
+        }
+        else if (cellsNumber == 81) {
+            newCell.classList.add('medium');
+        }
+        else if (cellsNumber == 49) {
+            newCell.classList.add('hard');
+        }
+
         newCell.addEventListener('click', function () {
 
 
